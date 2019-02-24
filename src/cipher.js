93a2911ad1cbbb76b2dc6c4002d4d4000eb67dd2
NadiaMon <arana.mo89@gmail.com>
2019-02-24 16:31:35 -0500

    agregando cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,24 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+
+  //EXPLICACIÓN DEL OFFSET Y STRING
+  encode:(offset,string) => {
+
+    let textNew=""; //almacenará texto codificado
+    for (let i = 0 ; i <string.length ; i++) {
+      if(string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
+        //almacenará el número ascii que le corresponde a los caracteres del texto a codificar
+        let asciiNumber =((string[i].charCodeAt())-65+ offset)%26+65;
+        //nuevas letras cifradas
+        let lettersNew = String.fromCharCode(asciiNumber);
+        textNew = textNew+lettersNew;
+      }else{
+        textNew=textNew+ string[i];
+    }
+    }
+
+return  textNew;
   },
+
   decode: () => {
     /* Acá va tu código */
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
+//declarar variables para llamar los input de texto y offset
+
+let captureText = document.getElementById ("ftext") // texto a cifrar
+let cipherOffset = document.getElementById ("firstOffset") // offset de cifrado
+let btnCipher = document.getElementById ("resultA") //botón de cifrado
+let cipherText = document.getElementById("fmessage")// imprimir el texto codificado
+let printText = document.getElementById ("stext"); // texto a descifrar
+let decipherOffset = document.getElementById ("secondOffset"); // offset de descifrar
+let btnDecipher = document.getElementById ("resultB") //botón de decifrado
+let decipherText = document.getElementById("smessage")// imprimir el texto decodificado
+
 //definir variables y funciones para cambiar de un div a otro
 
-const firstBtn = document.getElementById("cipher");
+const firstBtn = document.getElementById("optionCipher");
 firstBtn.addEventListener ("click", textCipher); //check
 
-const secondBtn = document.getElementById ("decipher");
+const secondBtn = document.getElementById ("optionDecipher");
 secondBtn.addEventListener ("click", textDecipher); //check
 
-const decipherTwo = document.getElementById ("decipher-b");
+const decipherTwo = document.getElementById ("decipherB");
 decipherTwo.addEventListener ("click", secondDecipher); //check
 
-const cipherTwo = document.getElementById ("cipher-b");
+const cipherTwo = document.getElementById ("cipherB");
 cipherTwo.addEventListener ("click", cipherDos); //check
 
 const exitPage = document.getElementById ("exit");
 //exitPage.addEventListener ("click", out); //mantener en espera, encontrar cómo salir de la página
 
-const exitTwo = document.getElementById ("exit-b");
+const exitTwo = document.getElementById ("exitB");
 //exitTwo.addEventListener ("click", secondOut); //mantener en espera, encontrar cómo salir de la página
 
 // para cambiar a arrow const name = () => {}
@@ -44,9 +55,21 @@ function cipherDos () {
     document.getElementById ("third").hidden = true;
 }
 
-//fórmula cifrado césar
+// crear función para botón cifrar
 
-const alphabet = ["A","B","C","D","E","F","G","H","I","J","K",
-"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const cipherClick = () => {
+
+let word = captureText.value.toUpperCase();
+let number = parseInt(cipherOffset.value);
+
+// esto dará el nuevo mensaje
+cipherText.value = cipher.encode(number,word);
+}
+
+
+//hacer que el algoritmo que sucede en el btn cifrar se
+//imprima en el cuadro
+btnCipher.addEventListener("click",cipherClick)
+//btnDecipher.addEventListener("click",decipherClick)
 
 
