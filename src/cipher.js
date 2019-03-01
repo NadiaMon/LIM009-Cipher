a9466ed4c683cfb86339e267986ecbc1c9700b4b
NadiaMon <arana.mo89@gmail.com>
2019-03-01 12:04:40 -0500

    cifrado y descifrado listo

--- src/cipher.js
+++ src/cipher.js
@@ -5,21 +5,31 @@ window.cipher = {
     let textNew=""; //almacenará texto codificado
     //bucle para recorrer cada letra en el texto
     for (let i=0;i<string.length;i++){
+      //crear condicional para atrapar el espacio 
+      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
         //almacenará el número ascii que le corresponde a los caracteres del texto a codificar
         let asciiNumber =((string[i].charCodeAt())-65+offset)%26+65;
         //nuevas letras cifradas
         let lettersNew = String.fromCharCode(asciiNumber);
         textNew = textNew+lettersNew;
+      }else{
+        textNew = textNew+string[i]; 
+      }
     }
 return textNew;
   },
 
-  decode: (offset, string) => {
+  decode: (offset,string) => {
     let newText="";
     for (let i=0;i<string.length;i++){
+      //número ascii del espacio 32, se crea la sig condición
+      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
       let ascii =((string[i].charCodeAt())+65-offset)%26+65;
       let letters = String.fromCharCode(ascii);
       newText = newText+letters; 
+    }else{
+      newText = newText+string[i]; 
+    }
     }
 return newText;
    }
